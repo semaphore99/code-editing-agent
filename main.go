b9e690df29a46ac8f3ee0b0555066db3b1ab983a
Dixon Deng <dixon.deng@gmail.com>
2025-06-10 22:27:57 +1000

    list files tool

--- main.go
+++ main.go
@@ -20,7 +20,7 @@ func main() {
 		return scanner.Text(), true
 	}
 
-	tools := []ToolDefinition{ReadFileDefinition}
+	tools := []ToolDefinition{ReadFileDefinition, ListFilesDefinition}
 
 	agent := NewAgent(&client, getUserMessage, tools)
 	err := agent.Run(context.TODO())
